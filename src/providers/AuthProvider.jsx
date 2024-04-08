b5406618751c3bd82ff8b5d9d0ade783593b6498
TI Sabbir <tisabbir0@gmail.com>
2024-04-09 03:56:03 +0600

    onAuthstateChanged added

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
-import { createContext, useState } from "react";
-import { GithubAuthProvider, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { GithubAuthProvider, GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import auth from "../firebase/firebase.config";
 
 export const AuthContext = createContext();
@@ -7,10 +7,29 @@ export const AuthContext = createContext();
 const AuthProvider = ({children}) => {
 
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     // Providers
     const googleProvider = new GoogleAuthProvider();
     const githubProvider = new GithubAuthProvider();
+
+
+    // setting the observer
+    useEffect(()=>{
+     const unsubscribe =   onAuthStateChanged(auth, (currentUser) => {
+            if (currentUser) {
+                console.log('from onAuthStateChanged',currentUser)
+              setUser(currentUser);
+            } else {
+              setUser(null);
+            }
+          });
+
+          return ()=>{
+            unsubscribe();
+          }
+    },[])
+    // setting the observer
 
     const createUser = (email, password) => {
       return  createUserWithEmailAndPassword(auth, email, password)
@@ -36,6 +55,8 @@ const AuthProvider = ({children}) => {
         logInUser,
         logInWithGoogle,
         logInWithGithub,
+        loading,
+        setLoading,
     }
     return (
         <AuthContext.Provider value={authInfo}>
