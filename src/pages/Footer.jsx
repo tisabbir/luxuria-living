779c4bd2b5bde3c8a899c5ca37d91f0ba0839913
TI Sabbir <tisabbir0@gmail.com>
2024-04-12 14:07:24 +0600

    Footer section with relevent information is added

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,4 +1,6 @@
 
+import { FaFacebook } from "react-icons/fa";
+import { FaAirbnb, FaGithub, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa6";
 
 const Footer = () => {
     return (
@@ -6,25 +8,35 @@ const Footer = () => {
             <footer className="footer p-10  max-w-6xl mx-auto">
   <nav>
     <h6 className="footer-title">Services</h6> 
-    <a className="link link-hover">Branding</a>
-    <a className="link link-hover">Design</a>
-    <a className="link link-hover">Marketing</a>
-    <a className="link link-hover">Advertisement</a>
+    <a className="link link-hover">Property Listings</a>
+    <a className="link link-hover">Property Management</a>
+    <a className="link link-hover">Investment Consultation</a>
+    <a className="link link-hover">Renovation and Construction</a>
   </nav> 
   <nav>
     <h6 className="footer-title">Company</h6> 
     <a className="link link-hover">About us</a>
     <a className="link link-hover">Contact</a>
-    <a className="link link-hover">Jobs</a>
-    <a className="link link-hover">Press kit</a>
+    <a className="link link-hover">Awards and Recognition</a>
+    <a className="link link-hover">Careers</a>
   </nav> 
   <nav>
     <h6 className="footer-title">Legal</h6> 
     <a className="link link-hover">Terms of use</a>
     <a className="link link-hover">Privacy policy</a>
     <a className="link link-hover">Cookie policy</a>
+    <a className="link link-hover">Copyright Notice</a>
   </nav> 
   <form>
+    <h6 className="footer-title">Social Media Links</h6> 
+    <div className="flex gap-2 mb-2">
+        <FaFacebook />
+        <FaTwitter />
+        <FaGithub />
+        <FaInstagram />
+        <FaLinkedin />
+        <FaAirbnb />
+    </div>
     <h6 className="footer-title">Newsletter</h6> 
     <fieldset className="form-control w-80">
       <label className="label">
